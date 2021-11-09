9c5d443e2742e043491ce40f559d3834e501c32f
Renan Souza <s.renan@outlook.com>
2021-11-08 21:52:11 -0300

    Requisito 6 - Botao Salvar

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      cartas: {},
     };
     this.onInputChange = this.onInputChange.bind(this);
   }
@@ -39,13 +40,18 @@ class App extends React.Component {
     });
   }
 
-  // onSaveButtonClick(event) {
-  //   event.preventDefault();
-  // }
-
   onSaveButtonClick = (event) => {
     event.preventDefault();
-    console.log('passei no onbutton click');
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+    });
   }
 
   render() {
@@ -64,6 +70,15 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
           isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
           onSaveButtonClick={ this.onSaveButtonClick }
